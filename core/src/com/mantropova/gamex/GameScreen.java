f918c2d4e1e9d76899e481bb503b5bdc524c5125
813192 <a-marry@mail.ru>
2019-04-15 22:46:28 +0300

    Template of game

--- core/src/com/mantropova/gamex/GameScreen.java
+++ core/src/com/mantropova/gamex/GameScreen.java
@@ -3,21 +3,26 @@ package com.mantropova.gamex;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mantropova.gamex.model.Beaver;
+
+import javax.xml.soap.Text;
 
 public class GameScreen implements Screen {
 
     private Texture beaverTexture;
     private SpriteBatch batch;
     private Beaver beaver;
+    private OrthographicCamera camera;
 
     @Override
     public void show() {
         batch = new SpriteBatch();
-        beaverTexture = new Texture(Gdx.files.internal("mole.png"));
-        beaver = new Beaver(beaverTexture, 0, 0, 64, 64);
+        beaverTexture = new Texture(Gdx.files.internal("Images/mole.png"));
+        beaverTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        beaver = new Beaver(beaverTexture, 0, 0, 2f, 2f * 1f);
     }
 
     @Override
@@ -25,6 +30,7 @@ public class GameScreen implements Screen {
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        batch.setProjectionMatrix(camera.combined);
         batch.begin();
         beaver.draw(batch);
         batch.end();
@@ -33,7 +39,11 @@ public class GameScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        //Соотношение ширины и высоты(сторон)
+        float aspectRatio = (float) height/width;
+        camera = new OrthographicCamera(20f, 20f * aspectRatio);
+        camera.zoom = 0.9f;
+        camera.update();
     }
 
     @Override
--- desktop/src/com/mantropova/gamex/desktop/DesktopLauncher.java
+++ desktop/src/com/mantropova/gamex/desktop/DesktopLauncher.java
@@ -1,5 +1,9 @@
 package com.mantropova.gamex.desktop;
 
+/**
+ * Created by Antropova Marina on 01.04.2019.
+ */
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mantropova.gamex.Main;
@@ -7,6 +11,14 @@ import com.mantropova.gamex.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = "FunnyBeavers";
+
+		config.width = 854;
+		config.height = 480;
+		config.x = 100;
+		config.y = 100;
+
 		new LwjglApplication(new Main(), config);
 	}
 }
