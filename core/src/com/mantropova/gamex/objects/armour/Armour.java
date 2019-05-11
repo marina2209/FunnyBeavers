5fbb30d147a0519211d78eb96a1e56dd95cb2acc
813192 <a-marry@mail.ru>
2019-05-11 22:53:55 +0300

    Add getters into Armour class

--- core/src/com/mantropova/gamex/objects/armour/Armour.java
+++ core/src/com/mantropova/gamex/objects/armour/Armour.java
@@ -4,8 +4,16 @@ package com.mantropova.gamex.objects.armour;
  * Created by Antropova Marina on 11.05.2019.
  */
 
-public class Armour {
-    public int moreHealth;
-    public int moreAttack;
+public abstract class Armour {
+    protected int moreHealth;
+    protected int moreAttack;
+
+    public int getMoreHealth() {
+        return moreHealth;
+    }
+
+    public int getMoreAttack() {
+        return moreAttack;
+    }
 }
 
