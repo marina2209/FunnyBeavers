ff6caece345ad769de72de257d10474786a368a7
813192 <a-marry@mail.ru>
2019-05-11 22:35:29 +0300

    Add Barrel class

---
+++ core/src/com/mantropova/gamex/objects/armour/Barrel.java
@@ -0,0 +1,15 @@
+package com.mantropova.gamex.objects.armour;
+
+import com.mantropova.gamex.objects.Player;
+
+/**
+ * Created by Antropova Marina on 12.05.2019.
+ */
+
+public class Barrel extends Armour {
+
+    public Barrel(Player player) {
+        moreAttack = 0;
+        moreHealth = 0;
+    }
+}
