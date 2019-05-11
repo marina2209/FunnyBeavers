437268ebd8a410a72cbf6189e372c455e1568ae5
813192 <a-marry@mail.ru>
2019-05-11 23:06:57 +0300

    Add Hammer

---
+++ core/src/com/mantropova/gamex/objects/weapons/Hammer.java
@@ -0,0 +1,25 @@
+package com.mantropova.gamex.objects.weapons;
+
+import com.mantropova.gamex.helpers.AssetsLoader;
+import com.mantropova.gamex.objects.Player;
+
+/**
+ * Created by Antropova Marina on 12.05.2019.
+ */
+
+public class Hammer extends  Weapon {
+    public Hammer(Player player) {
+        AssetsLoader assets = new AssetsLoader();
+        damage = assets.getPrefs().getInteger("HammerDamage");
+        level = assets.getPrefs().getInteger("HammerLevel");
+    }
+
+    @Override
+    public int attack() {
+        return damage * level;
+    }
+
+    @Override
+    public void extraAttack() {
+    }
+}
