a984d9b840a3c755065c6f9b7e847378e50627a4
813192 <a-marry@mail.ru>
2019-05-11 23:10:32 +0300

    Add Shovel

---
+++ core/src/com/mantropova/gamex/objects/weapons/Shovel.java
@@ -0,0 +1,31 @@
+package com.mantropova.gamex.objects.weapons;
+
+import com.mantropova.gamex.helpers.AssetsLoader;
+import com.mantropova.gamex.objects.Player;
+
+/**
+ * Created by Antropova Marina on 12.05.2019.
+ */
+
+
+public class Shovel extends Weapon {
+
+    private Player player;
+
+    public Shovel(Player player) {
+        AssetsLoader assets = new AssetsLoader();
+        this.player = player;
+        damage = assets.getPrefs().getInteger("ShovelDamage");
+        level = assets.getPrefs().getInteger("ShovelLevel");
+    }
+
+    @Override
+    public int attack() {
+        return damage * level;
+    }
+
+    @Override
+    public void extraAttack() {
+        if (player.health < 100) player.health += damage / 5;
+    }
+}
