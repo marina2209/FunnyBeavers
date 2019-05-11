ffd0374ea18d89fae87d7300955736df7a11e319
813192 <a-marry@mail.ru>
2019-05-11 22:34:08 +0300

    Add Armour base class

---
+++ core/src/com/mantropova/gamex/objects/armour/Armour.java
@@ -0,0 +1,11 @@
+package com.mantropova.gamex.objects.armour;
+
+/**
+ * Created by Antropova Marina on 11.05.2019.
+ */
+
+public class Armour {
+    public int moreHealth;
+    public int moreAttack;
+}
+
