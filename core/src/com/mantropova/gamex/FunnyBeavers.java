6c5810a7fddf8b71f1c095a0174708735fedb818
813192 <a-marry@mail.ru>
2019-05-14 21:40:02 +0300

    Add font

--- core/src/com/mantropova/gamex/FunnyBeavers.java
+++ core/src/com/mantropova/gamex/FunnyBeavers.java
@@ -3,6 +3,7 @@ package com.mantropova.gamex;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.mantropova.gamex.helpers.AssetsLoader;
 import com.mantropova.gamex.screens.MainMenuScreen;
@@ -11,6 +12,8 @@ public class FunnyBeavers extends Game {
 
 	public Skin skin = null;
     private Screen gameScreen;
+	public BitmapFont font = null;
+	public BitmapFont levels = null;
 
 	@Override
 	public void create() {
@@ -18,4 +21,11 @@ public class FunnyBeavers extends Game {
 		gameScreen = new MainMenuScreen();
 		setScreen(gameScreen);
 	}
+
+	@Override
+	public void render() {
+		super.render();
+	}
+
+	private static String FONT_CHARACTERS = "�������������������������������������Ũ��������������������������abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789][_!$%#@|\\/?-+=()*&.;,{}\"?`'<>";
 }
