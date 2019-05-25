4729eae8df006c8248eb7e40808ce1bb3e8dca3b
813192 <a-marry@mail.ru>
2019-05-25 22:15:18 +0300

    Finish to create the Player

--- core/src/com/mantropova/gamex/objects/Player.java
+++ core/src/com/mantropova/gamex/objects/Player.java
@@ -5,14 +5,32 @@ package com.mantropova.gamex.objects;
  */
 
 import com.badlogic.gdx.Gdx;
+import com.mantropova.gamex.objects.armour.Armour;
+import com.mantropova.gamex.objects.armour.Barrel;
+import com.mantropova.gamex.objects.weapons.Hammer;
+import com.mantropova.gamex.objects.weapons.Shovel;
+import com.mantropova.gamex.objects.weapons.Weapon;
 
 public class Player {
 
     public int health;
     public int attack;
     public int moleKilled;
+    private Armour armour;
+    private Weapon weapon;
 
     public Player(String weaponType) {
+        if (weaponType.equals("Hammer")) {
+            weapon = new Hammer(this);
+        } else if (weaponType.equals("Shovel")) {
+            weapon = new Shovel(this);
+        } else {
+            weapon = new Shovel(this);
+        }
+        armour = new Barrel(this);
+        health = 100 + armour.getMoreHealth();
+        attack = weapon.attack() + armour.getMoreAttack();
+
         moleKilled = 0;
     }
 
@@ -23,6 +41,7 @@ public class Player {
     }
 
     public int attack() {
+        weapon.extraAttack();
         return attack;
     }
 }
